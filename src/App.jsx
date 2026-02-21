f0ef3dcb900a68e16b7370b2722ff59ae6321d51
tsohal01 <TSOHAL01@GMAIL.COM>
2026-02-21 12:58:40 -0800

    Update import path for DMNBrainMap component

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import DMNBrainMap from "./dmn-brain-map-v6_1.jsx";
+import DMNBrainMap from "./dmn-brain-map-v6.jsx";
 
 export default function App() {
   return <DMNBrainMap />;
