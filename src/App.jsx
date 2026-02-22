af83be9257f712e04d283ba388a2b5f46ad32bf3
tsohal01 <TSOHAL01@GMAIL.COM>
2026-02-22 14:13:06 -0800

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import DMNBrainMap from "./brain_map_3.jsx";
+import DMNBrainMap from "./dmn-brain-map-v7.jsx";
 
 export default function App() {
   return <DMNBrainMap />;
